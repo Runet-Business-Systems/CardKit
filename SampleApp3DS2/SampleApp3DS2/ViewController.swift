16f01ff2879e123ea6d82d3b548ec338bbc0deeb
Alex Korotkov <alexkorotkovh@gmail.com>
2020-12-11 12:05:52 +0300

    Init SampleApp3DS2 project

---
+++ SampleApp3DS2/SampleApp3DS2/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  SampleApp3DS2
+//
+//  Created by Alex Korotkov on 12/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red;
+    }
+}
+
